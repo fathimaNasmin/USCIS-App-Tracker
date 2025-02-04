8eb118f33b0433479d1cdbb9ad56c2bb81b124ab
Nasmin <fathimanesmi@gmail.com>
2025-02-04 18:15:27 -0500

    Initial Commit

---
+++ USCISAppTracker/USCISAppTracker/USCISAppTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  USCISAppTrackerApp.swift
+//  USCISAppTracker
+//
+//  Created by Fathima Nasmin on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct USCISAppTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
