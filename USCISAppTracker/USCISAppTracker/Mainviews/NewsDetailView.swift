fad0c3d00031195eb9a189357283ae4cbec8e691
Nasmin <fathimanesmi@gmail.com>
2025-02-11 20:08:49 -0500

    created a new view for news detail

---
+++ USCISAppTracker/USCISAppTracker/Mainviews/NewsDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  NewsDetailView.swift
+//  USCISAppTracker
+//
+//  Created by Fathima Nasmin on 2/11/25.
+//
+
+import SwiftUI
+
+struct NewsDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewsDetailView()
+}
