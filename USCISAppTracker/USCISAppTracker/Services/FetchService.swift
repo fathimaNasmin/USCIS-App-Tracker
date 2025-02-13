388fc09ebcca7bb94ae5b442b3ea0407c9f1e145
Nasmin <fathimanesmi@gmail.com>
2025-02-13 18:05:00 -0500

    created a view model for response of case status

---
+++ USCISAppTracker/USCISAppTracker/Services/FetchService.swift
@@ -0,0 +1,8 @@
+//
+//  FetchService.swift
+//  USCISAppTracker
+//
+//  Created by Fathima Nasmin on 2/13/25.
+//
+
+import Foundation
---
+++ USCISAppTracker/USCISAppTracker/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  USCISAppTracker
+//
+//  Created by Fathima Nasmin on 2/13/25.
+//
+
+import Foundation
