452db345654207a143caf2c45de37d1abfc1fa45
Nasmin <fathimanesmi@gmail.com>
2025-03-13 09:04:57 -0400

    change in ui view in singleNewsView.

--- USCISAppTracker/USCISAppTracker/UI/Subviews/SingleNewsView.swift
+++ USCISAppTracker/USCISAppTracker/UI/Subviews/SingleNewsView.swift
@@ -29,23 +29,14 @@ struct SingleNewsView: View {
 				.frame(height: 1)
 				.frame(maxWidth: .infinity)
 				.foregroundColor(Color.textGray)
-			
-			Text(singleNews.title)
-				.subTitleStyle(18)
-				.fontWeight(.medium)
-				.padding(.bottom, 10)
-				.foregroundColor(.black)
-			
-			Text("Read More")
-				.titleStyle(18)
-				.frame(height: 35)
-				.frame(maxWidth: .infinity)
-				.foregroundColor(.white)
-				.background(.bluePrimary)
-				.clipShape(RoundedRectangle(cornerRadius: 20))
-				.onTapGesture {
-					selectedNewsId = 1
-				}
+			HStack {
+				Text(singleNews.title)
+					.subTitleStyle(16)
+					.fontWeight(.medium)
+					.padding(.bottom, 5)
+					.foregroundColor(.black)
+				Spacer()
+			}
 		}
 		.customBoxModifier(opacity:0.2, radius:3, x: 0, y: 2)
 	}
